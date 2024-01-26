c2d998234b594728743087cd3aba47c9688db0af
AsmaaMohammed95 <mazenzaki2022@gmail.com>
2024-01-26 11:47:15 -0800

    initilization

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,5 +12,9 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t -Typedef for struct dog
+*/
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif/*DOG_H*/
