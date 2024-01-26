a82692d9444ff00fae3c6aa7a57864743c020715
AsmaaMohammed95 <mazenzaki2022@gmail.com>
2024-01-26 08:38:04 -0800

    structure creation

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure type
+ * @name: a string of character
+ * @age: a flout variable
+ * @owner: a stringe of character
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif/*DOG_H*/
